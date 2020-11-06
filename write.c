2a6d9cdbe0e7cb1a4673e8d9548fa747600b75f4
arshiyazeba1 <73938527+arshiyazeba1@users.noreply.github.com>
2020-11-06 11:43:44 +0530

    Create write.c

---
+++ write.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#<sys/types.h>
+
+int main()
+{
+    int fd;
+    char buf[1024];
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
+    printf("Writer process started and now open one more terminal for running reader process\n");
+    fd = open(myfifo, O_WRONLY);
+    write(fd, "WELCOME E2 BATCH", sizeof("WELCOME E2 BATCH"));
+    close(fd);
+    unlink(myfifo);
+    return 0;
+}
